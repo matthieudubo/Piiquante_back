f49e133f15cab212b01e31017d7a46ed48bda5f4
Matthieu Dubo <matthieu.dubo40@gmail.com>
2023-05-15 10:09:23 +0200

    added express rate limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
 
 const userRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
@@ -16,10 +17,17 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", limiter, userRoutes);
 app.use("/api/sauces", saucesRoutes);
 
 app.use("/images", express.static(path.join(__dirname, "images")));
